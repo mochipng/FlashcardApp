c80d51662a8b64eb2fff59194140bffbc1ada7fe
mochipng <lilyfaithlord@gmail.com>
2025-09-03 13:39:42 -0400

    Add imports

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # 9/3/25, Desktop app where users can make flashcards with ease and practice them whenever
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QPushButton, QLabel, QMainWindow
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 import cardCreator
+
